a72dc5700f6c50b6caffccb00ba2c6bfc9e84595
zoro0307 <truongnhuhungnam@gmail.com>
2021-05-02 07:24:19 +0700

    update

--- store/todos/actions.js
+++ store/todos/actions.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
 export default {
-    getTodos({ commit }) {
-        axios
-            .get('https://jsonplaceholder.typicode.com/todos')
-            .then((response) => response.data)
-            .then((todos) => {
-                console.log(todos)
-                commit('SET_TODOS', todos)
-            })
+    async getTodos({ commit }) {
+        try {
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/todos'
+            )
+            commit('SET_TODOS', response.data)
+        } catch (error) {}
     },
 }
